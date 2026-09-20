2f9756d98596c4dca44df931d8512cdab5b4d9d2
Manasa S R <manasasrup8822@gmail.com>
2026-09-20 17:39:23 +0530

    Add Medium Java Solution for 71. Simplify Path from LeetCode

---
+++ LeetCode/Java/Medium/71. Simplify Path/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+
+        Stack<String> stack = new Stack<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (String folder : stack) {
+            result.append("/");
+            result.append(folder);
+        }
+
+        if (result.length() == 0) {
+            return "/";
+        }
+
+        return result.toString();
+    }
+}
